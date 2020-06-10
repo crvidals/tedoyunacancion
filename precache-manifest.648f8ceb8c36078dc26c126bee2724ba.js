4546fc16f208f32d34b4846f2d96e36d73c41993
Cristian Vidal <crvidals@gmail.com>
2020-06-09 21:14:44 -0400

    Updates

---
+++ precache-manifest.648f8ceb8c36078dc26c126bee2724ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f274e4d57cfd777f426fba4b498953de",
+    "url": "/tedoyunacancion/index.html"
+  },
+  {
+    "revision": "16c21ccd855b7f044f13",
+    "url": "/tedoyunacancion/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f4858b29168c1366f6a7",
+    "url": "/tedoyunacancion/static/js/2.7348d5f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tedoyunacancion/static/js/2.7348d5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c21ccd855b7f044f13",
+    "url": "/tedoyunacancion/static/js/main.344c6812.chunk.js"
+  },
+  {
+    "revision": "60f912574052459aed81",
+    "url": "/tedoyunacancion/static/js/runtime-main.d07adddd.js"
+  },
+  {
+    "revision": "3e9a49b6ba12718d8474219c847871e1",
+    "url": "/tedoyunacancion/static/media/tedoyunacancion.3e9a49b6.jpg"
+  }
+]);
